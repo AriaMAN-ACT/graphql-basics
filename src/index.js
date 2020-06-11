6577dd43c4d1e1a4f4f4197f0d13b86d06962d06
WolfMAN <aria.azadi.pour@protonmail.com>
2020-06-11 13:42:54 +0430

    create simple query

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("GraphQL");
+import {GraphQLServer} from "graphql-yoga";
+
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello() {
+            return 'Hello';
+        }
+    }
+};
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+server.start(() => {
+    console.log('server is up.')
+});
